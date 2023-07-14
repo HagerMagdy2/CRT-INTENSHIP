310b6bfa012166b3d489b85605126f3b2dbd265b
Hager Magdy <120759962+HagerMagdy2@users.noreply.github.com>
2023-07-14 15:44:52 +0300

    Add files via upload

---
+++ CRT/Task_2/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Night Changes',
+        path: 'musics/Song 1.mp3',
+        artist: 'One Direction',
+        cover: 'images/cover 10.png'
+    },
+    {
+        name: ' On & On',
+        path: 'musics/Song 2.mp3',
+        artist: 'Cartoon',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Until I Found You',
+        path: 'musics/Song 3.mp3',
+        artist: 'Stephen Sanchez, Em Beihold',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Flowers',
+        path: 'musics/Song 4.mp3',
+        artist: 'Miley Cyrus',
+        cover: 'images/cover 40.png'
+    },
+    {
+        name: 'Perfect',
+        path: 'musics/Song 5.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'images/cover 5.png'
+    },
+]
